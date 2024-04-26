6949187a1d54346d1f739c85129b3a45d6a9ae65
Sushil <sushilkumar811544@gmail.com>
2024-04-26 15:16:05 +0530

    Rendered JSX

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+//React.createElement ==> ReactElement-JS Object ==> HTMLElement(render)
+
 const heading = React.createElement(
     "h1", 
     { id: "heading"},
      "Namaste React🚀"
      );
-     
-const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+
+//JSX ==> React.createElement ==> ReactElement-JS Object ==> HTMLElement(render)
+
+const jsxHeading = <h1 id="heading">Namaste React using JSX🚀🚀</h1>
+console.log(jsxHeading);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(jsxHeading);
