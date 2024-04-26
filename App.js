b558bd3e1b88b758ed12ffe64eb5c015a9841aea
Sushil <sushilkumar811544@gmail.com>
2024-04-26 15:54:44 +0530

    destructured a little..but still static

--- App.js
+++ App.js
@@ -19,7 +19,7 @@ const Header = () => {
 };
 
 const RestaurantCard = (props) => {
-    console.log(props);
+    const {resName, cuisine} = props;
     return (
         <div className="res-card" style={{backgroundColor: "#f0f0f0"}}>
            <img
@@ -27,10 +27,10 @@ const RestaurantCard = (props) => {
             alt="res-logo" 
             src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/c99751d54e4e1847186c62b3309c1327"
            />
-            <h3>{props.resName}</h3>
+            <h3>{resName}</h3>
             <h4>*4</h4>
             <h4>25-30 mins</h4>
-            <p>{props.cuisine}</p>
+            <p>{cuisine}</p>
         </div>
     );
 };
@@ -40,7 +40,6 @@ const Body = () => {
             <div className="search">
                 Search
             </div>
-
             <div className="res-container">
                 <RestaurantCard
                     resName="Big Bowl"
