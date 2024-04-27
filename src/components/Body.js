a2aebdccbc0ffdcf1d9559ac070b50cacc490d10
Sushil <sushilkumar811544@gmail.com>
2024-04-27 15:58:31 +0530

    implemented Reconcilliation and toLowerCase to add Search button

--- src/components/Body.js
+++ src/components/Body.js
@@ -5,9 +5,32 @@ import Shimmer from "./Shimmer";
 
 const Body = () => {
     const [listOfRestaurants,setListOfRestaurants] = useState(resList);
+    const [searchText, setSearchText] = useState("");
     return listOfRestaurants.length === 0 ? (<Shimmer /> ) : (
        <div className="body">
         <div className="filter">
+            <div className="search">
+                    <input type="text" className="search-box"
+                     value={searchText}
+                     onChange={(e)=> {
+                        setSearchText(e.target.value);
+                     }} 
+                    />
+                    <button className="search-btn" onClick={
+                        () => {
+                            //filter the restaurant cart and update thr UI
+                            //searchText
+                            console.log(searchText);
+
+                            const filteredRestaurant = listOfRestaurants.filter(
+                                (res) => res.info.name.toLowerCase().includes(searchText.toLowerCase())
+                            );
+                            setListOfRestaurants(filteredRestaurant);
+                        }
+                    }>
+                        Search
+                    </button>
+                </div>
             <button className="filter-btn"
             onClick={()=>{
                  const filteredData = resList.filter(
